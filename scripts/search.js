523149e197a8b623ab89f699334f19d6a4501638
Julien MAVEAU <julien.maveau@gmail.com>
2025-11-18 14:09:47 +0100

    modification index et search

--- scripts/search.js
+++ scripts/search.js
@@ -1,1 +1,16 @@
-f
+
+
+
+function searchbar() {
+    let input = document.getElementsByClassName('.Search-word').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('');
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "list-item";
+        }
+    }
+}
