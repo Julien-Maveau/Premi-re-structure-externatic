5a2250afc72b1177a84df9703c95c6ed2163ad47
Julien MAVEAU <julien.maveau@gmail.com>
2025-11-20 13:24:41 +0100

    puull

--- scripts/highlight.js
+++ scripts/highlight.js
@@ -1,26 +1,4 @@
 
-// recuperer le fichier JSON
-const file = "../Components/technologie.json";
-// recuperer la data du fichier JSON
-
-fetch(file)
-    .then(response => response.json())
-    .then((datatech) => {
-        const list = document.querySelector(".list");
-        list.innerHTML = "";
-        //  iterer sur 
-        datatech.forEach((name) => {
-            //  pour chacune des  , on crée le html
-            console.log(name)
-            const namelist = '<li class="${card-identity}"> ${name}</li>'
-            //  Recuperer le div 
-            // Inserer le html de chaque   dans le html
-            list.insertAdjacentHTML("beforeend", name);
-
-        })
-    })
-
-
 
 
 
--- scripts/search.js
+++ scripts/search.js
@@ -21,3 +21,25 @@ function searchbar() {
 // récupérer le form
 // écouter le submit du form
 // appeler la fonction search bar
+// recuperer le fichier JSON
+const file = "../Components/technologie.json";
+// recuperer la data du fichier JSON
+
+fetch(file)
+    .then(response => response.json())
+    .then((datatech) => {
+        const list = document.querySelector(".list");
+
+        //  iterer sur 
+        datatech.forEach((name) => {
+            //  pour chacune des  , on crée le html
+            console.log(name);
+            const namelist = '<li id="${card-identity}" class="list"> ${name}</li>'
+            //  Recuperer le div 
+            // Inserer le html de chaque   dans le html
+            list.insertAdjacentHTML("beforeend", name);
+            console.log(namelist)
+
+        })
+    })
+
